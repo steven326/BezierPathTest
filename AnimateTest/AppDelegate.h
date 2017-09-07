9eb33c5d3e579d5c3141ccdea00b386522a6b4b7
王泽平 <wangzp@cfilmcloud.com>
2017-09-07 16:29:08 +0800

    Initial Commit

---
+++ AnimateTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimateTest
+//
+//  Created by Steven on 2017/9/7.
+//  Copyright © 2017年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimateTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimateTest
+//
+//  Created by Steven on 2017/9/7.
+//  Copyright © 2017年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
